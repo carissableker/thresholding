e0c3b8db972a85aeae958e7954f158bff32bddf4
carissableker <carissableker@users.noreply.github.com>
2020-03-18 14:52:53 -0400

    no clique number

--- src/maximal_cliques.cpp
+++ src/maximal_cliques.cpp
@@ -1,4 +1,4 @@
-#include "maximal_cliques.h" 
+#include "maximal_cliques.h"
 
 int maximal_cliques(igraph_t& G,
 					int minimum_cliquesize,
@@ -6,7 +6,7 @@ int maximal_cliques(igraph_t& G,
                     igraph_integer_t& clique_number){
 
     igraph_maximal_cliques_count(&G, &clique_count, minimum_cliquesize, 0);
-    igraph_clique_number(&G, &clique_number);
- 
+    //igraph_clique_number(&G, &clique_number);
+
     return 0;
 }
